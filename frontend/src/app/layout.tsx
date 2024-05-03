721f9dabf014df69dde45041de9db17458a07198
Dev Env <dev@dev-env.develop>
2024-05-02 20:13:28 -0400

    initial fe/be setup

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Providers } from "./providers";
+import { cookieToInitialState } from "@alchemy/aa-alchemy/config";
+import { headers } from "next/headers";
+import { config } from "./config";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,9 +18,15 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  //allows us to persist state across page boundaries
+  const initialState = cookieToInitialState(config, headers().get("cookie"));
+
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Providers initialState={initialState}>{children}</Providers>
+      </body>
     </html>
   );
 }
