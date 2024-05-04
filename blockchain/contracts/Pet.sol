2a510a245471407b0b6848f5ac7ab1243f3a940c
chambsbond <jacob.chambers.ceng@gmail.com>
2024-05-04 14:31:18 -0500

    Fixed nft contract

--- blockchain/contracts/Pet.sol
+++ blockchain/contracts/Pet.sol
@@ -2,35 +2,17 @@
 pragma solidity ^0.8.20;
 
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import {ERC721Enumerable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import {MintClaimModel} from "../models/MintClaimModel.sol";
-import {Chainlink, ChainlinkClient} from "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
-import {ConfirmedOwner} from "@chainlink/contracts/src/v0.8/shared/access/ConfirmedOwner.sol";
-import {LinkTokenInterface} from "@chainlink/contracts/src/v0.8/shared/interfaces/LinkTokenInterface.sol";
 import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
 
-contract Animal is ERC721URIStorage, ChainlinkClient, ConfirmedOwner {
-    using Chainlink for Chainlink.Request;
-
-    bytes32 private jobId;
-    uint256 private fee;
-
-    event RequestId(bytes32 indexed requestId, string uuid);
-    
+contract Animal is ERC721 {
+    event ClaimMade(address orgAffiliation, address claimee, address prospectOwner);    
     uint256 private _nextTokenId;
-    uint256 private _nextClaimId;
-
     address immutable private  _orgRegistryAddress;
-    mapping(uint256 => MintClaimModel) private _mintMap;
 
-    constructor(address orgRegistryAddress) ERC721("Animal", "PET") ConfirmedOwner(msg.sender)
+    constructor(address orgRegistryAddress) ERC721("Animal", "PET")
     {
-        //todo reset these addresses
-        _setChainlinkToken(0x779877A7B0D9E8603169DdbD7836e478b4624789);
-        _setChainlinkOracle(0x6090149792dAAeE9D1D568c9f9a6F6B46AA29eFD);
-        jobId = "ca98366cc7314957b8c012c72f05aeeb";
-        fee = (1 * LINK_DIVISIBILITY) / 10; // 0,1 * 10**18 (Varies by network and job)
         _orgRegistryAddress = orgRegistryAddress;
     }
 
@@ -44,76 +26,17 @@ contract Animal is ERC721URIStorage, ChainlinkClient, ConfirmedOwner {
         return _nextTokenId;
     }
 
-    function claimOffChainId(uint256 claimId) private returns(bytes32 requestId)
-    {
-        Chainlink.Request memory req = _buildChainlinkRequest(
-            jobId,
-            address(this),
-            this.fulfill.selector
-        );
-
-        // Set the URL to perform the GET request on
-        req._add(
-            "get",
-            string.concat("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=ETH&tsyms=USD", Strings.toString(claimId))
-        );
-
-        // Set the path to find the desired data in the API response, where the response format is:
-        // {
-        //    "id": some uuid,
-        //    "claimId: claimid
-        // }
-        // request.add("path", "RAW.ETH.USD.VOLUME24HOUR"); // Chainlink nodes prior to 1.0.0 support this format
-        req._add("path", "id"); // Chainlink nodes 1.0.0 and later support this format
-        req._add("path", "claimId");
-
-        // Sends the request
-        return _sendChainlinkRequest(req, fee);
-    }
-
-    /**
-     * Receive the response in the form of uint256
-    */
-    function fulfill(
-        bytes32 _requestId,
-        string memory id,
-        uint256 claimId
-    ) public recordChainlinkFulfillment(_requestId) {
-        emit RequestId(_requestId, id);
-        
-        this.mint(claimId, id);
-    }
-
-    function mintClaim(address orgAffiliation, address claimee, address prospectOwner)
+    function mint(address orgAffiliation, address claimee, address prospectOwner)
         public
         onlyRegistry
         returns (uint256)
     {
-        MintClaimModel memory claimModel;
-        uint256 claimId = _nextClaimId++;
-
-        claimModel.claimee = claimee;
-        claimModel.orgAffiliation = orgAffiliation;
-        claimModel.prospectOwner = prospectOwner;
-
-        //Todo use queue for cleanup??
-        _mintMap[claimId] = claimModel;
-
-        return claimId;
-    }
-
-    function mint(uint256 claimId, string memory externalId)
-        public
-        onlyRegistry
-        returns (uint256)
-    {
+        emit ClaimMade(orgAffiliation, claimee, prospectOwner);
         uint256 tokenId = _nextTokenId++;
 
         //TODO validate prospect owner can recieve prior to this call
-        _safeMint(_mintMap[claimId].prospectOwner, tokenId);    
+        _safeMint(prospectOwner, tokenId);    
      
-        _setTokenURI(tokenId, externalId);
-
         return tokenId;
     }
 
