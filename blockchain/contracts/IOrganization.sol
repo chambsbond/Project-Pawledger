1d9017aa01cece1f2e1863ea4f2e3fc9a4883578
liam rethore <me@bliamrethore.com>
2024-05-05 13:32:03 -0700

    Add suggested interface to orgs too.

--- blockchain/contracts/IOrganization.sol
+++ blockchain/contracts/IOrganization.sol
@@ -2,8 +2,9 @@
 pragma solidity ^0.8.24;
 
 import "../model/Employee.sol";
+import {IERC721Receiver} from "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 
-interface IOrganization {
+interface IOrganization is IERC721Receiver {
     function addEmployee(Employee memory employee) external;
 
     function removeEmployee(address employee) external;
