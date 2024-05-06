d8f6cab2b4bf36b0354d3420f3f56fcee5d41fd4
chambsbond <jacob.chambers.ceng@gmail.com>
2024-05-05 19:33:52 -0500

    Format baseOrg

--- blockchain/packages/hardhat/contracts/BaseOrganization.sol
+++ blockchain/packages/hardhat/contracts/BaseOrganization.sol
@@ -5,29 +5,27 @@ import "./IOrganization.sol";
 import "../models/Employee.sol";
 
 abstract contract BaseOrganization is IOrganization {
-    address private _owner;
-    mapping(address => Employee) private _employees;
+	address private _owner;
+	mapping(address => Employee) private _employees;
 
-    constructor(address owner, Employee[] memory employees) 
-    {
-        _owner = owner;
-        for (uint i = 0; i < employees.length; i++) {
-          _employees[employees[i].walletAddress] = employees[i];
-        }
-    }
+	constructor(address owner, Employee[] memory employees) {
+		_owner = owner;
+		for (uint i = 0; i < employees.length; i++) {
+			_employees[employees[i].walletAddress] = employees[i];
+		}
+	}
 
-    function addEmployee(Employee memory employee) external override 
-    {
-        _employees[employee.walletAddress] = employee;
-    }
+	function addEmployee(Employee memory employee) external override {
+		_employees[employee.walletAddress] = employee;
+	}
 
-    function removeEmployee(address employeeAddress) external override 
-    {
-        delete _employees[employeeAddress];
-    }
+	function removeEmployee(address employeeAddress) external override {
+		delete _employees[employeeAddress];
+	}
 
-    function isEmployee(address employeeAddress) external view override returns(bool)
-    {
-        return _employees[employeeAddress].walletAddress != address(0x0);
-    }
+	function isEmployee(
+		address employeeAddress
+	) external view override returns (bool) {
+		return _employees[employeeAddress].walletAddress != address(0x0);
+	}
 }
