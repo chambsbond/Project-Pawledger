cef319326fb4ad6edd6df21b8a93d0191b58e474
liam rethore <me@bliamrethore.com>
2024-05-26 16:07:35 -0700

    Deploy secrets in new project.

---
+++ blockchain/packages/chainlink-secrets/deploy/00_deploy_your_contract.ts
@@ -0,0 +1,48 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { SubscriptionManager, SecretsManager } from "@chainlink/functions-toolkit";
+import  { ethers } from "ethers";
+
+const deployYourContract = async function (hre: HardhatRuntimeEnvironment) {
+
+  const gatewayUrls = [
+    "https://01.functions-gateway.testnet.chain.link/",
+    "https://02.functions-gateway.testnet.chain.link/",
+  ];
+
+  const provider = new ethers.providers.JsonRpcProvider("	https://rpc-amoy.polygon.technology");
+  const wallet = new ethers.Wallet(process.env.DEPLOYER_PRIVATE_KEY as string);
+  const signer = wallet.connect(provider);
+  const routerAddress = "0xC22a79eBA640940ABB6dF0f7982cc119578E11De";
+  const donId = "fun-polygon-amoy-1";
+  
+  // First encrypt secrets and upload the encrypted secrets to the DON
+  const secretsManager = new SecretsManager({
+    signer: signer,
+    functionsRouterAddress: routerAddress,
+    donId: donId,
+  });
+
+  await secretsManager.initialize();
+
+  const secrets = { privateKey : process.env.DECRYPT_PRIVATE_KEY }
+  // Encrypt secrets and upload to DON
+  await secretsManager.initialize();
+  const encryptedSecretsObj = await secretsManager.encryptSecrets(secrets as Record<string,string>);
+  
+  const uploadResult = await secretsManager.uploadEncryptedSecretsToDON({
+    encryptedSecretsHexstring: encryptedSecretsObj.encryptedSecrets,
+    gatewayUrls: gatewayUrls,
+    slotId: 0,
+    minutesUntilExpiration: 15,
+  });
+  
+  if (!uploadResult.success)
+    throw new Error(`Encrypted secrets not uploaded to ${gatewayUrls}`);
+
+  console.log(
+    `\n✅ Secrets uploaded properly to gateways ${gatewayUrls}! Gateways response: `,
+    uploadResult
+  );
+};
+
+export default deployYourContract;
---
+++ blockchain/packages/chainlink-secrets/hardhat.config.ts
@@ -0,0 +1,48 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+import { HardhatUserConfig } from "hardhat/config";
+import "hardhat-deploy";
+
+// If not set, it uses ours Alchemy's default API key.
+// You can get your own at https://dashboard.alchemyapi.io
+const providerApiKey = process.env.ALCHEMY_API_KEY || "oKxs-03sij-U_N0iOlrSsZFr29-IqbuF";
+// If not set, it uses the hardhat account 0 private key.
+const deployerPrivateKey =
+  process.env.DEPLOYER_PRIVATE_KEY ?? "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
+// If not set, it uses ours Etherscan default API key.
+const etherscanApiKey = process.env.ETHERSCAN_API_KEY || "DNXJA8RX2Q3VZ4URQIWP7Z68CJXQZSC6AW";
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.19",
+    settings: {
+      optimizer: {
+        enabled: true,
+        // https://docs.soliditylang.org/en/latest/using-the-compiler.html#optimizer-options
+        runs: 200,
+      },
+    },
+  },
+  defaultNetwork: "localhost",
+  namedAccounts: {
+    deployer: {
+      // By default, it will take the first Hardhat account as the deployer
+      default: 0,
+    },
+  },
+  networks: {
+    polygonAmoy: {
+      url: `https://polygon-amoy.g.alchemy.com/v2/${providerApiKey}`,
+      accounts: [deployerPrivateKey],
+    },
+    polygonZkEvm: {
+      url: `https://polygonzkevm-mainnet.g.alchemy.com/v2/${providerApiKey}`,
+      accounts: [deployerPrivateKey],
+    },
+    polygonZkEvmTestnet: {
+      url: `https://polygonzkevm-testnet.g.alchemy.com/v2/${providerApiKey}`,
+      accounts: [deployerPrivateKey],
+    },
+  }
+};
+export default config;
