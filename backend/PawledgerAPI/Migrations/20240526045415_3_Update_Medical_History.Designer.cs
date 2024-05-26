3d35793947a265d3b621168a832cb011f16131c3
Liam Rethore <me@bliamrethore.com>
2024-05-25 21:56:03 -0700

    Apply EF Migration

--- backend/PawledgerAPI/Migrations/20240526045415_3_Update_Medical_History.Designer.cs
+++ backend/PawledgerAPI/Migrations/20240526045415_3_Update_Medical_History.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using PawledgerAPI.Context;
@@ -9,9 +10,10 @@ using PawledgerAPI.Context;
 namespace PawledgerAPI.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20240526045415_3_Update_Medical_History")]
+    partial class _3_Update_Medical_History
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ backend/PawledgerAPI/Migrations/20240526045415_3_Update_Medical_History.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace PawledgerAPI.Migrations
+{
+    public partial class _3_Update_Medical_History : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_MedicalHistory_Pet_PetTokenId",
+                table: "MedicalHistory");
+
+            migrationBuilder.DropIndex(
+                name: "IX_MedicalHistory_PetTokenId",
+                table: "MedicalHistory");
+
+            migrationBuilder.DropColumn(
+                name: "PetTokenId",
+                table: "MedicalHistory");
+
+            migrationBuilder.DropColumn(
+                name: "ResponseBytes",
+                table: "MedicalHistory");
+
+            migrationBuilder.RenameColumn(
+                name: "CreatedTs",
+                table: "Pet",
+                newName: "CreatedTimestamp");
+
+            migrationBuilder.RenameColumn(
+                name: "CreatedTs",
+                table: "MedicalHistory",
+                newName: "UpdatedTimestamp");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "MedicalHistoryId",
+                table: "MedicalHistory",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text")
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedTimestamp",
+                table: "MedicalHistory",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "EncryptedHistory",
+                table: "MedicalHistory",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedTimestamp",
+                table: "MedicalHistory");
+
+            migrationBuilder.DropColumn(
+                name: "EncryptedHistory",
+                table: "MedicalHistory");
+
+            migrationBuilder.RenameColumn(
+                name: "CreatedTimestamp",
+                table: "Pet",
+                newName: "CreatedTs");
+
+            migrationBuilder.RenameColumn(
+                name: "UpdatedTimestamp",
+                table: "MedicalHistory",
+                newName: "CreatedTs");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MedicalHistoryId",
+                table: "MedicalHistory",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint")
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PetTokenId",
+                table: "MedicalHistory",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ResponseBytes",
+                table: "MedicalHistory",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MedicalHistory_PetTokenId",
+                table: "MedicalHistory",
+                column: "PetTokenId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MedicalHistory_Pet_PetTokenId",
+                table: "MedicalHistory",
+                column: "PetTokenId",
+                principalTable: "Pet",
+                principalColumn: "TokenId",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
