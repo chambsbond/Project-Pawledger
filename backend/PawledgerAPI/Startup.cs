6c50d32db5f932c926a0118c86719b5896463f69
chambsbond <jacob.chambers.ceng@gmail.com>
2024-05-31 11:54:27 -0500

    Removing hsts

--- backend/PawledgerAPI/Startup.cs
+++ backend/PawledgerAPI/Startup.cs
@@ -48,7 +48,7 @@ namespace PawledgerAPI
       {
         app.UseExceptionHandler("/Home/Error");
         // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-        app.UseHsts();
+        // app.UseHsts();
       }
       app.UseHttpsRedirection();
       app.UseStaticFiles();
